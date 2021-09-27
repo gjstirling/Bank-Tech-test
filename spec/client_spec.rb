ca33dbb2b8b409e3af95aacae6da62bb8b8c029d
Graeme Stirling <graemejstirling@gmail.com>
2021-09-27 16:44:17 +0100

    Adding deposit method first test

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,5 +1,7 @@
 describe Client do 
 
+  let (:client) {described_class.new("Johnny Cash")}
+
   it "Raises Error when incorrect data type is used" do 
     expect { Client.new(123) }.to raise_error "Name information entered incorrectly"
   end 
@@ -13,9 +15,18 @@ describe Client do
     expect { Client.new("J0hn Do3!") }.to raise_error "Name cannot contain numbers or special characters"
   end  
   
-  
   it "Creates a new client when supplied a name" do 
     new_account = Client.new("John Doe")
     expect(new_account.name).to eq("John Doe")
   end 
+
+  describe "#deposit" do 
+
+    it "Stores money when called" do 
+      client.deposit(49.99)
+      expect(client.balance).to eq(49.99)
+
+    end 
+  end 
+  
 end 
