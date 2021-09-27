6bd43c0365241baca43473a6b45e7dbc06d64b8f
Graeme Stirling <graemejstirling@gmail.com>
2021-09-27 16:15:49 +0100

    Testing for empty string field

--- lib/client.rb
+++ lib/client.rb
@@ -3,7 +3,8 @@ class Client
   attr_reader :name 
 
   def initialize(name, balance = 0)
-    fail "Name information entered incorrectly" if !name.is_a?(String) 
+    fail "Name information entered incorrectly" if !name.is_a?(String)
+    fail "Name required to create account" if name.strip == ""
     @name = name
     @balance = balance
   end 
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,8 +1,13 @@
 describe Client do 
 
-  it "Raises Error when new client has no name or incorrect data type is used" do 
-    new_account = Client.new("")
+  it "Raises Error when incorrect data type is used" do 
     expect { Client.new(123) }.to raise_error "Name information entered incorrectly"
+  end 
+
+  it "Raises error when no name is given" do 
+    expect { Client.new("") }.to raise_error "Name required to create account"
+    expect { Client.new("   ") }.to raise_error "Name required to create account"
+
   end 
   
   it "Creates a new client when supplied a name" do 
