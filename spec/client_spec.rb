10a9283028c5a839202ce4638166d0248be45e16
Graeme Stirling <graemejstirling@gmail.com>
2021-09-27 16:24:40 +0100

    Testing for  incorrect format for name

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,8 +7,13 @@ describe Client do
   it "Raises error when no name is given" do 
     expect { Client.new("") }.to raise_error "Name required to create account"
     expect { Client.new("   ") }.to raise_error "Name required to create account"
-
   end 
+  
+  it "Gives error is string entered doesn't appear correct" do 
+    expect { Client.new("J0hn Do3") }.to raise_error "Name cannot contain numbers"
+    expect { Client.new("John Doe!") }.to raise_error "Name cannot contain special characters"
+  end  
+  
   
   it "Creates a new client when supplied a name" do 
     new_account = Client.new("John Doe")
